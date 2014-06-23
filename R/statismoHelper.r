77740041de69286efd2822ec69e634d95817aeda
schlager <zarquon42@gmail.com>
2014-06-23 13:07:41 +0200

    added option array for meshalign to return aligned meshes

--- R/statismoHelper.r
+++ R/statismoHelper.r
@@ -9,17 +9,28 @@ array2meshlist <- function(x) {
 #' align meshes stored in a list by their vertices
 #' @param meshlist list containing triangular meshes of class "mesh3d"
 #' @param scale logical: request scaling during alignment
+#' @param array logical: if TRUE the superimposed vertices will be returned as 3D array.
+#' @return returns a list of aligned meshes or an array of dimensions k x 3 x n, where k=number of vertices and n=sample size.
 #' @importFrom Morpho vert2points ProcGPA
 #' @export
-meshalign <- function(meshlist,scale=TRUE,tol=1e-5) {
+meshalign <- function(meshlist,scale=TRUE,array=FALSE) {
     vertarr <- meshlist2array(meshlist)
-    out <- ProcGPA(vertarr,scale=scale, CSinit = FALSE,silent = F,tol=tol)$rotated
-    return(out)
+    out <- rigidAlign(vertarr,scale=scale)$rotated
+    if (array) {
+        return(out)
+    } else {
+        out <- lapply(1:length(meshlist),function(i){ res <- meshlist[[i]]
+                                                      res$vb[1:3,] <- t(out[,,i])
+                                                      res$normals <- NULL
+                                                      return(res)})
+        names(out) <- names(meshlist)
+        return(out)
+    }
 }
 
-#' convert meshes to array consisting of vertexc coordinates
+#' convert meshes to array consisting of vertex coordinates
 #'
-#' convert meshes to array consisting of vertexc coordinates
+#' convert meshes to array consisting of vertex coordinates
 #' @param meshlist list containing triangular meshes of class "mesh3d"
 #' @return returns an array with k x 3 x n dimensions where k=number of vertices, and n=sample size.
 #' @importFrom Morpho vert2points
@@ -35,6 +46,7 @@ meshlist2array <- function(meshlist) {
         dimnames(vertarr)[[3]] <- paste("specimen",1:n,sep="_")
     return(vertarr)
 }
+
 checkmeshlist <- function(x) {
     verts <- unlist(lapply(x,function(y) y <- ncol(y$vb)))
     chk <- prod(verts==verts[1])
