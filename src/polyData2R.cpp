9a0caf84b97dbd89ce9749f25cf0ec6d03fd9e4e
schlager <zarquon42@gmail.com>
2014-06-25 18:01:18 +0200

    removed unnecessary include polyData.cpp

--- src/polyData2R.cpp
+++ src/polyData2R.cpp
@@ -1,5 +1,5 @@
 #include "polyData2R.h"
-#include <vtkSmartPointer.h>
+
 Rcpp::List polyData2R(vtkSmartPointer<vtkPolyData> polydata) {
   try {
     vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
@@ -14,7 +14,6 @@ Rcpp::List polyData2R(vtkSmartPointer<vtkPolyData> polydata) {
       for (int j=0; j<3; j++)
 	vb(j,i) = point[j];
     }
-  
     int h;
     vtkIdType npts=3,*pts; 
     int nit = polydata->GetNumberOfPolys();
