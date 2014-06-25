10bf36713c5d85c1ec55d6849f2d0c1fa454c625
schlager <zarquon42@gmail.com>
2014-06-25 18:00:59 +0200

    added #define in VTKheaders

--- inst/include/VTKTypes.h
+++ inst/include/VTKTypes.h
@@ -3,6 +3,9 @@
 #include "statismo/StatisticalModel.h"
 #include "statismo/DataManager.h"
 #include <memory>
+
+#ifndef _VTK_TYPES_H__
+#define _VTK_TYPES_H__
 using namespace statismo;
 using std::auto_ptr;
 
@@ -10,3 +13,4 @@ typedef vtkStandardMeshRepresenter RepresenterType;
 typedef DataManager<vtkPolyData> DataManagerType;
 typedef StatisticalModel<vtkPolyData> StatisticalModelType;
 
+#endif
