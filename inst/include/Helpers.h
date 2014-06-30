1ff306571a9da28d827aa21037ca542d22bb0cc2
schlager <zarquon42@gmail.com>
2014-06-30 15:51:34 +0200

    fixed Helpers.h

--- inst/include/Helpers.h
+++ inst/include/Helpers.h
@@ -1,7 +1,9 @@
 #ifndef _HELPERS_H__
 #define _HELPERS_H__
 #include "VTKTypes.h"
-
+#include "Rcpp.h"
+using namespace Rcpp;
+using namespace statismo;
 
 vtkPoint NumVec2vtkPoint(NumericVector vec);
 
