e497c134aa89dca0e4d281b0db88bd2e0e6308dc
schlager <zarquon42@gmail.com>
2014-08-05 15:32:11 +0200

    added statismoModelFromRepresenter

---
+++ R/statismoModelFromRepresenter.r
@@ -0,0 +1,33 @@
+#' generate model from a representer using gaussian kernels
+#'
+#' generate model from a representer using gaussian kernels
+#'
+#' @param representer mesh3d or matrix used as representer
+#' @param kernel a list containing two valued vectors containing with the first entry specifiying the bandwidth and the second the scaling of the Gaussian kernels.
+#' @param ncomp integer: number of PCs to approximate
+#' @param nystroem number of samples to compute Nystroem approximation of eigenvectors
+#' @return returns a shape model of class \code{\link{pPCA}}
+#' @examples
+#' require(Rvcg)
+#' data(humface)
+#' hummodel <- statismoModelFromRepresenter(humface)
+#' require(rgl)
+#' for (i in 1:5) wire3d(DrawSample(hummodel),col=i)
+#' @export
+statismoModelFromRepresenter <- function(representer,kernel=list(c(100,70)),ncomp=10,nystroem=500) {
+    if (inherits(representer,"mesh3d"))
+        center <- as.vector(representer$vb[1:3,])
+    else if (is.matrix(representer))
+        center <- as.vector(t(representer))
+    else
+        stop("representer must be mesh or matrix")
+    pp <- new("pPCA")
+    pp@sigma <- 0
+    pp@PCA$center <- as.vector(representer$vb[1:3,])
+    pp@PCA$sdev <- 1
+    pp@representer <- representer
+    pp@PCA$rotation <- matrix(0,length(pp@PCA$center),1)
+    out <- statismoGPmodel(pp,useEmpiric=FALSE,kernel=kernel,ncomp=ncomp,nystroem=nystroem)
+    return(out)
+}
+    
