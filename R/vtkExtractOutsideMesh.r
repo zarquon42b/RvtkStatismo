29b89032d043f18d1a370afa6be1ba48d4d8ca35
schlager <zarquon42@gmail.com>
2014-08-12 15:09:48 +0200

    added docu for vtkExtractOutsideMesh and vtkSurfaceReko

--- R/vtkExtractOutsideMesh.r
+++ R/vtkExtractOutsideMesh.r
@@ -1,12 +1,27 @@
+#' get outer mesh
+#'
+#' get outer mesh and discard enclosed segments
+#' @param x mesh
+#' @return returns only those segments visible from the outside of the mesh
+#' 
 #' @importFrom Morpho meshcube
+#' @export
 vtkExtractOutsideMesh <- function(x) {
     bbox <- as.matrix(meshcube(x))
     out <- .Call("vtkExtractOutsideMesh",x,bbox)
     out$vb <- rbind(out$vb,1)
     return(out)
 }
-vtkDelaunay3D <- function(x) {
-    
+
+#' create surface from pointcloud
+#'
+#' create surface from pointcloud or mesh
+#' @param x pointcloud or mesh
+#' @return mesh of class mesh3d
+#' @export
+vtkSurfaceReko <- function(x) {
+    if (is.matrix(x))
+        x <- list(vb=t(x))
     out <- .Call("vtkSurfaceReko",x)
     out$vb <- rbind(out$vb,1)
     return(out)
--- R/vtkMeshVolume.r
+++ R/vtkMeshVolume.r
@@ -1,9 +1,25 @@
+#' get Infos about a triangular mesh
+#'
+#' get Infos about a triangular mesh
+#' @param mesh triangular mesh
+#' @return
+#' \item{volume}{enclosed volume}
+#' \item{projVol}{enclosed projected volume}
+#' \item{surfaceArea}{surface area}
+#' @details see documentation of vtkMassProperties for details
 #' @export
 vtkMeshInfo <- function(mesh) {
     out <- .Call("vtkMeshInfo",mesh)
     return(out)
 }
 
+#' fill holes in triangular mesh
+#'
+#' fill holes in triangular mesh
+#' @param mesh triangular mesh
+#' @param holesize maximal size of holes to fill
+#' @return mesh with filled holes
+#' @details see documentation of vtkFillHolesFilter for details
 #' @export
 vtkFillHoles <- function(mesh,holesize=1e6) {
     out <- .Call("vtkFillHole",mesh,holesize)
