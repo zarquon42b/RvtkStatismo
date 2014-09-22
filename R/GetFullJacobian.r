2326759eef2ca90f73c71538207cf416fe47d9a9
schlager <zarquon42@gmail.com>
2014-09-22 14:02:58 +0200

    added GetFullJacobian

---
+++ R/GetFullJacobian.r
@@ -0,0 +1,24 @@
+#' get Jacobian for all model points or a subset
+#'
+#' get Jacobian for all model points or a subset
+#'
+#' @param model
+#' @param pt integer vector specifying which points to use
+#' @return
+#' returns a matrix with rows n: (n+2) containing the Jacobian for the n-th point
+#' @seealso \code{\link{GetJacobian}}
+#' @export
+#' 
+setGeneric("GetFullJacobian", function(model,pt) {
+    standardGeneric("GetFullJacobian")
+})
+setMethod("GetFullJacobian", signature(model="pPCA"), function(model,pt) {
+    if (missing(pt))
+        pt <- GetDomainPoints(model)
+    else
+        pt <- GetDomainPoints(model)[pt,]
+    # lse
+      
+    out <- .Call("GetFullJacobian",model,pt)
+    return(out)
+})
