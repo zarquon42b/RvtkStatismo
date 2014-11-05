89a01405603458ec53a61eae86020b0ed76fc99a
schlager <zarquon42@gmail.com>
2014-11-05 17:21:15 +0100

    bumped version to daily

--- R/RvtkStatismo-package.R
+++ R/RvtkStatismo-package.R
@@ -5,8 +5,8 @@
 #' \tabular{ll}{
 #' Package: \tab RvtkStatismo\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.2.140701\cr
-#' Date: \tab 2014-07-01\cr
+#' Version: \tab 0.2.141105\cr
+#' Date: \tab 2014-11-05\cr
 #' License: \tab GPL\cr
 #' LazyLoad: \tab yes\cr }
 #' 
