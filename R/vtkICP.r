1226193645d036d95de1e5367ea67df124496f12
Stefan Schlager <zarquon42@gmail.com>
2015-06-23 14:01:44 +0200

    made vtkICP public

--- R/vtkICP.r
+++ R/vtkICP.r
@@ -1,3 +1,13 @@
+#' Iteratively closest point registration
+#'
+#' Iteratively closest point registration
+#' @param refmesh moving mesh
+#' @param tarmesh fixed mesh
+#' @param iteration integer: number of iterations to run
+#' @param center logical: rotate meshes around center of gravity.
+#' @param type type of transform to be used: "none"=rigid,"scale"=similarity transform, "affine"=affine transform.
+#' @param sample number of random samples to use
+#' @export
 vtkICP <- function(refmesh,tarmesh,iterations=10,center=FALSE,type=c("none","scale", "affine"),sample=200) {
     type <- tolower(substr(type[1],1L,1L))
     
