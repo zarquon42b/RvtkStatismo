ca1e37e6402ddd2f2418a85ed74011cae426a5c5
Stefan Schlager <zarquon42@gmail.com>
2015-06-30 14:46:41 +0200

    added option reflection to meshalign and rigidAlign

--- R/alignment.r
+++ R/alignment.r
@@ -6,11 +6,12 @@
 #' @param use.lm integer vector: specifies the indices of the points that are to be used in the constrained model
 #' @param deselect logical: if TRUE, \code{use.lm} references the missing coordinates instead of the present ones.
 #' @param reference integer: instead of a GPA, all specimens are aligned to this specific one.
+#' @param reflection logical: if TRUE, reflections are allowed to minimzie squared distance.
 #' @return a list containing
 #' \item{rotated}{array containing registered coordinates}
 #' \item{mshape}{matrix containing meanshape}
 #' @export
-rigidAlign <- function(array,scale=FALSE,use.lm=NULL,deselect=FALSE,reference=NULL) {
+rigidAlign <- function(array,scale=FALSE,use.lm=NULL,deselect=FALSE,reference=NULL,reflection=FALSE) {
     k <- dim(array)[1]
     if (!is.null(use.lm)) {
         use.lm <- unique(sort(use.lm))
@@ -18,27 +19,27 @@ rigidAlign <- function(array,scale=FALSE,use.lm=NULL,deselect=FALSE,reference=NU
             use.lm <- c(1:k)[-use.lm]
         }
     }
-    out <- partialAlign(array,use.lm = use.lm,scale=scale,reference=reference)
+    out <- partialAlign(array,use.lm = use.lm,scale=scale,reference=reference,reflection=reflection)
 }
 
-partialAlign <- function(array,use.lm=NULL,scale=FALSE,reference=NULL) {
+partialAlign <- function(array,use.lm=NULL,scale=FALSE,reference=NULL,reflection=FALSE) {
     if (is.null(reference)){
         if (!is.null(use.lm)){
-            procMod <- ProcGPA(array[use.lm,,],scale=scale,CSinit=F,reflection=F,silent = TRUE)##register all data using Procrustes fitting based on the non-missing coordinates
+            procMod <- ProcGPA(array[use.lm,,],scale=scale,CSinit=F,reflection=reflection,silent = TRUE)##register all data using Procrustes fitting based on the non-missing coordinates
             tmp <- array
             a.list <-  1:(dim(array)[3])
-            tmp <- lapply(a.list, function(i) {mat <- rotonmat(array[,,i],array[use.lm,,i],procMod$rotated[,,i],scale=scale,reflection = F);return(mat)})
+            tmp <- lapply(a.list, function(i) {mat <- rotonmat(array[,,i],array[use.lm,,i],procMod$rotated[,,i],scale=scale,reflection = reflection);return(mat)})
             tmp1 <- Morpho::list2array(tmp)
             procMod$rotated <- tmp1
             procMod$mshape <- arrMean3(tmp1)
         } else {
-            procMod <- ProcGPA(array,scale=scale,CSinit = F,reflection = F,silent = T)
+            procMod <- ProcGPA(array,scale=scale,CSinit = F,reflection = reflection,silent = T)
         }
     } else {
         if (is.null(use.lm))
             use.lm <- 1:dim(array)[1]
         a.list <-  1:(dim(array)[3])
-        tmp <- lapply(a.list, function(i) {mat <- rotonmat(array[,,i],array[use.lm,,i],array[,,reference],scale=scale,reflection = F);return(mat)})
+        tmp <- lapply(a.list, function(i) {mat <- rotonmat(array[,,i],array[use.lm,,i],array[,,reference],scale=scale,reflection = reflection);return(mat)})
         procMod <- list()
         procMod$rotated <- Morpho::list2array(tmp)
         procMod$mshape <- arrMean3(procMod$rotated)
@@ -57,22 +58,31 @@ partialAlign <- function(array,use.lm=NULL,scale=FALSE,reference=NULL) {
 #' @param array logical: if TRUE the superimposed vertices will be returned as 3D array.
 #' @return returns a list of aligned meshes or an array of dimensions k x 3 x n, where k=number of vertices and n=sample size.
 #' @param reference integer: instead of a GPA, all specimens are aligned to this specific one.
+#' @param reflection logical: if TRUE, reflections are allowed to minimzie squared distance.
 #' @examples
 #' mod <- statismoLoadModel(system.file("extdata","mandibles.h5",package="RvtkStatismo"))
 #' shapes <- restoreSamples(mod)
 #' newalign <- meshalign(shapes,reference=1)
 #' @importFrom Morpho vert2points ProcGPA
 #' @export
-meshalign <- function(meshlist,scale=FALSE,use.lm=NULL,deselect=FALSE,array=FALSE,reference=NULL) {
+meshalign <- function(meshlist,scale=FALSE,use.lm=NULL,deselect=FALSE,array=FALSE,reference=NULL,reflection=FALSE) {
     vertarr <- meshlist2array(meshlist)
-    out <- rigidAlign(vertarr,scale=scale,use.lm=use.lm,deselect=FALSE,reference=reference)$rotated
+    out <- rigidAlign(vertarr,scale=scale,use.lm=use.lm,deselect=FALSE,reference=reference,reflection=reflection)$rotated
     if (array) {
         return(out)
     } else {
-        out <- lapply(1:length(meshlist),function(i){ res <- meshlist[[i]]
-                                                      res$vb[1:3,] <- t(out[,,i])
-                                                      res$normals <- NULL
-                                                      return(res)})
+        if (is.null(reference)) {
+            out <- lapply(1:length(meshlist),function(i){ res <- meshlist[[i]]
+                                                          res$vb[1:3,] <- t(out[,,i])
+                                                          res$normals <- NULL
+                                                          return(res)})
+        } else {
+            out <- lapply(1:length(meshlist),function(i){ res <- meshlist[[i]]
+                                                          res$vb[1:3,] <- t(out[,,i])
+                                                          res$it <- meshlist[[reference]]$it
+                                                          res$normals <- NULL
+                                                          return(res)})
+        }
         names(out) <- names(meshlist)
         return(out)
     }
