740217fdfe8659f0f3c76e075b3f59a396650049
schlager <zarquon42@gmail.com>
2019-11-15 11:08:12 +0100

    restoreSamples: add option to only use the first n PCs to obtain shapes

--- R/restoreSamples.r
+++ R/restoreSamples.r
@@ -3,6 +3,7 @@
 #' restores original samples (meshes or pointclouds) from a model with stored PC-scores
 #'
 #' @param model shape model model of class "pPCA"
+#' @param n only use the first n PCscores
 #' @return returns a list of restored shapes
 #' @note the exact original shapes are only returned in case the model's assumed noise variance sigma = 0 (as eigenvalues might be discarded.
 #' @examples
@@ -17,10 +18,16 @@
 #' deformGrid3d(rest[[1]],align[,,1])
 #' }
 #' @export
-restoreSamples <- function(model) {
+restoreSamples <- function(model,n=NULL) {
     out <- list()
-    scores <- GetPCScores(model,TRUE)    
-    for (i in 1:nrow(scores))
+    scores <- GetPCScores(model,TRUE)
+    if (is.null(n))
+        n <- 1:nrow(scores)
+    else {
+        if (max(n) > nrow(scores))
+            stop(paste0("n must be in the range of 1:",nrow(scores)))
+    }
+    for (i in n)
         out[[i]] <- DrawSample(model,coefficients=scores[i,])
     names(out) <- rownames(scores)
     return(out)
